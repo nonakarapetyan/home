1676472b02f3c43663a2377b120af0f5a473cd4e
nonakarapetyan <nona.karapetyan.98@mail.ru>
2019-05-31 15:10:27 +0500

    home

---
+++ home.js
@@ -0,0 +1,12 @@
+// function factorial(a) {
+//     if (a===1){
+//         return a
+//     }
+//     return a* factorial(a-1)
+// }
+// console.log(factorial(5))
+
+var text='close this page'
+ alert(text.charAt(0).toUpperCase()+text.charAt(1)+text.charAt(2)+text.charAt(3)+text.charAt(4)
+ +text.charAt(5)+text.charAt(6).toUpperCase()+text.charAt(7)+text.charAt(8)+text.charAt(9)
+ +text.charAt(10)+text.charAt(11).toUpperCase()+text.charAt(12)+text.charAt(13)+text.charAt(14))
